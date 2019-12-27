5e2bdee59913153a8999d6d8440e8bf59bced8ab
Maciej Krzemianowski <git@easiestsolution.eu>
2019-12-27 10:21:53 +0100

    Add cache files & set magit status global shortcut.

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -6,5 +6,6 @@
 ;; (projectile-mode +1)
 (define-key evil-normal-state-map (kbd "SPC p") 'projectile-switch-project)
 (define-key evil-normal-state-map (kbd "SPC SPC") 'projectile-find-file)
+(global-set-key (kbd "C-x g") 'magit-status)
 
 (provide 'base-global-keys)
