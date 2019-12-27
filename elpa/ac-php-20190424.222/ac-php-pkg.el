8be20b3e5e0ad9c18a9415a7e61db7e7411e5e79
Maciej Krzemianowski <git@easiestsolution.eu>
2019-12-27 20:56:16 +0100

    Further cleanups.

--- elpa/ac-php-20190424.222/ac-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ac-php" "20190424.222" "Auto Completion source for PHP." '((ac-php-core "2.0") (auto-complete "1.4.0") (yasnippet "0.8.0")) :commit "84aa4f0c4ffafa2c2fdfbcb16662abac0a571013" :keywords '("completion" "convenience" "intellisense") :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :url "https://github.com/xcwen/ac-php")
--- elpa/dracula-theme-20191029.1905/dracula-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dracula-theme" "20191029.1905" "Dracula Theme" '((emacs "24")) :commit "f8a7f8461c963a9b8e5629ae2bab547d81aad0a6" :authors '(("film42")) :maintainer '("film42") :url "https://github.com/dracula/emacs")
